77409b208dffc8d147a7c282aebb1e8b7402c019
Oleh_Kovtoniuk <kovtoniuk.oleh@gmail.com>
2025-06-19 23:31:54 +0200

    виправлено axios замість fetch в файлі pixabay-api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,22 @@
+import axios from 'axios';
+
 const API_KEY = '50866984-6402497f149a27b4d4d3393ca';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function getImagesByQuery(query, page = 1) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=15`;
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: 15,
+  };
 
   try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
   } catch (error) {
     console.error('Error fetching images:', error);
     return { hits: [], totalHits: 0 };
